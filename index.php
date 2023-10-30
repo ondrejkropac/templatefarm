f6dd083308a60403e485867cb2f5a0ae414c0490
ondrejkropac <148766369+ondrejkropac@users.noreply.github.com>
2023-10-30 11:51:59 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+// !! Tento soubor musí být v kořenové složce hostingu! Jinak web nebude fungovat !!
+
+/*  _____ _______         _                      _
+ * |_   _|__   __|       | |                    | |
+ *   | |    | |_ __   ___| |___      _____  _ __| | __  ___ ____
+ *   | |    | | '_ \ / _ \ __\ \ /\ / / _ \| '__| |/ / / __|_  /
+ *  _| |_   | | | | |  __/ |_ \ V  V / (_) | |  |   < | (__ / /
+ * |_____|  |_|_| |_|\___|\__| \_/\_/ \___/|_|  |_|\_(_)___/___|
+ *                                _
+ *              ___ ___ ___ _____|_|_ _ _____
+ *             | . |  _| -_|     | | | |     |  LICENCE
+ *             |  _|_| |___|_|_|_|_|___|_|_|_|
+ *             |_|
+ *
+ * IT ZPRAVODAJSTVÍ  <>  PROGRAMOVÁNÍ  <>  HW A SW  <>  KOMUNITA
+ *
+ * Tento zdrojový kód je součástí výukových seriálů na
+ * IT sociální síti WWW.ITNETWORK.CZ
+ *
+ * Kód spadá pod licenci prémiového obsahu a vznikl díky podpoře
+ * našich členů. Je určen pouze pro osobní užití a nesmí být šířen.
+ * Více informací na http://www.itnetwork.cz/licence
+ */
+
+session_start();
+
+// Nastavení interního kódování pro funkce pro práci s řetězci
+mb_internal_encoding("UTF-8");
+
+/**
+ * Callback pro automatické načítání tříd controllerů a modelů
+ * @param string $trida Název třídy k načtení
+ * @return void
+ */
+function autoloadFunkce(string $trida) : void
+{
+	// Končí název třídy řetězcem "Kontroler" ?
+    if (preg_match('/Kontroler$/', $trida))	
+        require("kontrolery/" . $trida . ".php");
+    else
+        require("modely/" . $trida . ".php");
+}
+
+// Registrace callbacku (Pod starým PHP 5.2 je nutné nahradit fcí __autoload())
+spl_autoload_register("autoloadFunkce");
+
+// Připojení k databázi
+Db::pripoj("127.0.0.1", "root", "", "templates");
+
+// Vytvoření routeru a zpracování parametrů od uživatele z URL
+$smerovac = new SmerovacKontroler();
+$smerovac->zpracuj(array($_SERVER['REQUEST_URI']));
+
+// Vyrenderování šablony
+$smerovac->vypisPohled();
